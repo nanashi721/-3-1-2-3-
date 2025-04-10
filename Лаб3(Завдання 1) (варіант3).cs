a0292b19786ebd7c88441c0c24fe400e62496a8a
nanashi721 <153307563+nanashi721@users.noreply.github.com>
2025-04-10 10:42:20 +0300

    Add files via upload

---
+++ Лаб3(Завдання 1) (варіант3).cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Parallelogram
+{
+    private double sideA, sideB, angleDegrees;
+
+    public Parallelogram(double sideA, double sideB, double angleDegrees)
+    {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.angleDegrees = angleDegrees;
+    }
+
+    public double GetPerimeter()
+    {
+        return 2 * (sideA + sideB);
+    }
+
+    public double GetArea()
+    {
+        double radians = angleDegrees * Math.PI / 180.0;
+        return sideA * sideB * Math.Sin(radians);
+    }
+
+    public double GetDiagonal()
+    {
+        double radians = angleDegrees * Math.PI / 180.0;
+        return Math.Sqrt(sideA * sideA + sideB * sideB - 2 * sideA * sideB * Math.Cos(radians));
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Parallelogram parallelogram = new Parallelogram(5, 3, 60);
+
+        Console.WriteLine("Периметр паралелограма: " + parallelogram.GetPerimeter());
+        Console.WriteLine("Площа паралелограма: " + parallelogram.GetArea());
+        Console.WriteLine("Довжина діагоналі: " + parallelogram.GetDiagonal());
+
+        Console.ReadLine();
+}
---
+++ Лаб3(Завдання 2) (варіант3).cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Product
+{
+    private double pricePerUnit;
+    private int quantity;
+
+    public Product(double pricePerUnit, int quantity)
+    {
+        this.pricePerUnit = pricePerUnit;
+        this.quantity = quantity;
+    }
+
+    public string GetInfo()
+    {
+        return $"Ціна за одиницю: {pricePerUnit} грн, Кількість: {quantity}";
+    }
+
+    public double CalculateTotalCost()
+    {
+        return pricePerUnit * quantity;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Product product = new Product(45.50, 3);
+        Console.WriteLine(product.GetInfo());
+        Console.WriteLine("Загальна вартість товару: " + product.CalculateTotalCost() + " грн");
+    }
+}
